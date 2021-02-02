8f65a5d2c2d523d0edce3da18a0fd6f96bcffb33
zhangpeng <wa_wj999@163.com>
2021-02-02 22:23:45 +0800

    新增了excel03和07版本生成excel的代码

--- kakapoi/src/main/java/com/zhang/ExceWriteTest.java
+++ kakapoi/src/main/java/com/zhang/ExceWriteTest.java
@@ -1,11 +1,71 @@
 package com.zhang;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.joda.time.DateTime;
 import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 public class ExceWriteTest {
 
+    String path = "E:\\develop\\kaka-excel\\kakapoi\\";
+
     @Test
-    public void testWrite03(){
-        System.out.println("hello poi");
+    public void testWrite03() throws Exception {
+
+        //创建工作簿
+        Workbook workbook = new HSSFWorkbook();
+        //创建工作表
+        Sheet sheet = workbook.createSheet("狂神观众统计表");
+        Row row1 = sheet.createRow(0);
+        Cell cell11 = row1.createCell(0);
+        cell11.setCellValue("今日新增观众");
+        Cell cell12 = row1.createCell(1);
+        cell12.setCellValue(666);
+
+        Row row2 = sheet.createRow(1);
+        Cell cell21 = row2.createCell(0);
+        cell21.setCellValue("统计时间");
+        Cell cell22 = row2.createCell(1);
+        String time = new DateTime().toString("yyyy-MM-dd HH:mm:ss");
+        cell22.setCellValue(time);
+
+        //生成excel，excel03结尾是xls
+        FileOutputStream outputStream = new FileOutputStream(path + "kaka.xls");
+        workbook.write(outputStream);
+        outputStream.close();
+        System.out.println("kaka.xls生成完毕");
+    }
+    @Test
+    public void testWrite07() throws Exception {
+
+        //创建工作簿
+        Workbook workbook = new XSSFWorkbook();
+        //创建工作表
+        Sheet sheet = workbook.createSheet("狂神观众统计表");
+        Row row1 = sheet.createRow(0);
+        Cell cell11 = row1.createCell(0);
+        cell11.setCellValue("今日新增观众");
+        Cell cell12 = row1.createCell(1);
+        cell12.setCellValue(666);
+
+        Row row2 = sheet.createRow(1);
+        Cell cell21 = row2.createCell(0);
+        cell21.setCellValue("统计时间");
+        Cell cell22 = row2.createCell(1);
+        String time = new DateTime().toString("yyyy-MM-dd HH:mm:ss");
+        cell22.setCellValue(time);
+
+        //生成excel，excel07结尾是xlsx
+        FileOutputStream outputStream = new FileOutputStream(path + "kaka.xlsx");
+        workbook.write(outputStream);
+        outputStream.close();
+        System.out.println("kaka.xlsx生成完毕");
     }
 }
